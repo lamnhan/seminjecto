99b2262794f665c1ceac1d1446320d9e38cacdfd
Lam Nhan <lamnhan92@gmail.com>
2020-10-13 18:08:37 +0700

    fix new command naming

--- src/lib/services/create.service.ts
+++ src/lib/services/create.service.ts
@@ -1,5 +1,6 @@
 import {resolve} from 'path';
 import axios from 'axios';
+import {camelCase, capitalCase, pascalCase} from 'change-case';
 
 import {FileService} from './file.service';
 import {DownloadService} from './download.service';
@@ -39,25 +40,38 @@ export class CreateService {
     // modify content
     const name = (dest.replace(/\\/g, '/').split('/').pop() as string)
       .toLowerCase()
-      .replace(/[\W_]+/g, '');
-    const titleName = name.charAt(0).toUpperCase() + name.substr(1);
-    await this.modifyContent(type, dest, name, titleName, description);
+      .replace(/[^a-zA-Z-]/g, ' ')
+      .replace(/ /g, '-');
+    const nameCamel = camelCase(name);
+    const namePascal = pascalCase(name);
+    const nameCapital = capitalCase(name);
+    await this.modifyContent(
+      type,
+      dest,
+      name,
+      nameCamel,
+      namePascal,
+      nameCapital,
+      description
+    );
   }
 
   private async modifyContent(
     type: CreateType,
     dest: string,
     name: string,
-    titleName: string,
+    nameCamel: string,
+    namePascal: string,
+    nameCapital: string,
     description: string
   ) {
     // src/public-api.ts
     await this.fileService.changeContent(
       resolve(dest, 'src', 'public-api.ts'),
       {
-        '{Lib as LibModule}': `{Lib as ${titleName}Module}`,
-        '{Cli as LibCliModule}': `{Cli as ${titleName}CliModule}`,
-        '{App as LibAppModule}': `{App as ${titleName}AppModule}`,
+        '{Lib as LibModule}': `{Lib as ${namePascal}Module}`,
+        '{Cli as LibCliModule}': `{Cli as ${namePascal}CliModule}`,
+        '{App as LibAppModule}': `{App as ${namePascal}AppModule}`,
       }
     );
     // LIB
@@ -80,12 +94,12 @@ export class CreateService {
       await this.fileService.changeContent(
         resolve(dest, 'src', 'cli', 'index.ts'),
         {
-          '{Lib as LibModule}': `{Lib as ${titleName}Module}`, // import {...}
+          '{Lib as LibModule}': `{Lib as ${namePascal}Module}`, // import {...}
           "'cli'": `'${name}'`,
           "'A Seminjecto project.'": `'${description}'`,
-          'libModule: LibModule': `${name}Module: ${titleName}Module`, // private ...
-          'this.libModule': `this.${name}Module`,
-          'new LibModule': `new ${titleName}Module`,
+          'libModule: LibModule': `${nameCamel}Module: ${namePascal}Module`, // private ...
+          'this.libModule': `this.${nameCamel}Module`,
+          'new LibModule': `new ${namePascal}Module`,
         }
       );
     }
@@ -100,10 +114,10 @@ export class CreateService {
       await this.fileService.changeContent(
         resolve(dest, 'src', 'app', 'index.ts'),
         {
-          '{Lib as LibModule}': `{Lib as ${titleName}Module}`, // import {...}
-          'libModule: LibModule': `${name}Module: ${titleName}Module`, // private ...
-          'this.libModule': `this.${name}Module`,
-          'new LibModule': `new ${titleName}Module`,
+          '{Lib as LibModule}': `{Lib as ${namePascal}Module}`, // import {...}
+          'libModule: LibModule': `${nameCamel}Module: ${namePascal}Module`, // private ...
+          'this.libModule': `this.${nameCamel}Module`,
+          'new LibModule': `new ${namePascal}Module`,
         }
       );
     }
@@ -118,10 +132,10 @@ export class CreateService {
       await this.fileService.changeContent(
         resolve(dest, 'src', 'app', 'index.ts'),
         {
-          '{Lib as LibModule}': `{Lib as ${titleName}Module}`, // import {...}
-          'libModule: LibModule': `${name}Module: ${titleName}Module`, // private ...
-          'this.libModule': `this.${name}Module`,
-          'new LibModule': `new ${titleName}Module`,
+          '{Lib as LibModule}': `{Lib as ${namePascal}Module}`, // import {...}
+          'libModule: LibModule': `${nameCamel}Module: ${namePascal}Module`, // private ...
+          'this.libModule': `this.${nameCamel}Module`,
+          'new LibModule': `new ${namePascal}Module`,
         }
       );
     }
@@ -136,7 +150,7 @@ export class CreateService {
       await this.fileService.changeContent(
         resolve(dest, 'src', 'addon', 'index.ts'),
         {
-          "createMenu('Addon')": `createMenu('${titleName}')`,
+          "createMenu('Addon')": `createMenu('${nameCapital}')`,
         }
       );
     }
